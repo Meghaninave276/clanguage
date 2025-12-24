8173bba70bf40fa8549eb30bea90d57b52760f52
Megha <meghaninave8@gmail.com>
2025-12-24 09:13:55 +0530

    first commit

---
+++ datatype&variable/three.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b;
+   
+    printf("enter a:");
+    scanf("%d",&a);
+     printf("enter b:");
+    scanf("%d",&b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    
+ 
+
+    printf("a= %d\n",a);
+    printf("b= %d",b);
+
+
+}
+//10,20
+//20
+//10
+   
