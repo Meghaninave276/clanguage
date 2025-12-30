b7cae745a7991f85ee8441d443422f5e80d3517e
Megha ninave <meghaninave8@gmail.com>
2025-12-30 11:01:57 +0530

    Add files via upload

---
+++ oprators/one.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int a,b;
+	printf("enter a:");
+	scanf("%d",&a);
+	printf("enter b:");
+	scanf("%d",&b);
+	int sum=a+b;
+	printf("sum is %d\n",sum);
+	int sub=a-b;
+	printf("sub is %d\n",sub);
+	int mul=a*b;
+	printf("mul is %d\n",mul);
+	int div=a/b;
+	printf("div is %d",div);
+	return 0;
+	
+	
+}
