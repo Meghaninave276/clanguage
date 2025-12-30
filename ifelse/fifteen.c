b3f48bea789386ca8d930a40e6aa3ab9a53a9ab4
Megha ninave <meghaninave8@gmail.com>
2025-12-30 11:22:12 +0530

    Add files via upload

---
+++ ifelse/fifteen.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+ 	int a,b,c;
+ 	printf("enter a:");
+	scanf("%d",&a);
+	printf("enter b:");
+	scanf("%d",&b);
+	printf("enter c:");
+	scanf("%d",&c);
+	//a,b,c
+	a>b?a>c?printf("a is max"):printf("c is max"):
+	b>c?printf("b is max"):printf("c is max");
+	return 0;
+ }
