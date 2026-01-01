f5d6df920940c1f1d2edec517caf0a98374d4243
Megha ninave <meghaninave8@gmail.com>
2026-01-01 09:52:31 +0530

    Update one.c

--- indiabix/one.c
+++ indiabix/one.c
@@ -1,13 +1,28 @@
+// #include<stdio.h>
+// #include<conio.h>
+// int main()
+// {
+// 	float a, i=2;
+// 	for(a=1; a<=8; a++)
+// 	{
+// 		printf("%f ",i);
+// 		i/=2;
+		
+		
+// 	}
+// }
+
 #include<stdio.h>
 #include<conio.h>
 int main()
 {
-	float a, i=2;
-	for(a=1; a<=8; a++)
+	int a=2,i;
+	for(i=2; i<=8; i*=2)
 	{
-		printf("%f ",i);
-		i/=2;
+		printf(" 1/ %d ",+i);
+		
 		
 		
 	}
 }
+
